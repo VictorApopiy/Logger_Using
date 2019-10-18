6ecad8e92d68f40a7696b7545752c5ee60d86bae
viktor.apopii <viktor.apopii@eleks.com>
2019-10-18 17:02:10 +0300

    fixed casting, codestyle, defines

--- LoggerUse/LoggerUse.cpp
+++ LoggerUse/LoggerUse.cpp
@@ -4,36 +4,42 @@
 #include <stdio.h>
 #include <Windows.h>
 #include "iloggerapi.h"
-typedef ILoggerApi* (__cdecl* IloggerApi_factory)();
 
 int main()
 {
-	HINSTANCE dll_handler;
-	dll_handler = LoadLibrary(TEXT("logger.dll"));
-	if (dll_handler != NULL)
+	do
 	{
-		printf("dll loaded successfully \n");
-		IloggerApi_factory LoggerApiInit = (IloggerApi_factory)::GetProcAddress((HMODULE)dll_handler, "CreateLogger");
-		if (LoggerApiInit != NULL)
+		int IResult = 0;
+		HINSTANCE dll_handler;
+		dll_handler = LoadLibrary(TEXT("logger.dll"));
+		if (dll_handler != NULL)
 		{
-			printf ("Getting a ptr to func is successful \n");
-			ILoggerApi* pinstance = LoggerApiInit();
-			pinstance->WriteMessage();
-			pinstance->DeleteInstance();
+			printf("dll loaded successfully \n");
+			IloggerApi_factory LoggerApiInit = (IloggerApi_factory)::GetProcAddress((HMODULE)dll_handler, "CreateLogger");
+			if (LoggerApiInit != NULL)
+			{
+				printf("Getting a ptr to func is successful \n");
+				ILoggerApi* pinstance = LoggerApiInit();
+				pinstance->WriteMessage();
+				pinstance->DeleteInstance();
+			}
+			else
+			{
+				printf("Getting func ptr is failed \n");
+				::FreeLibrary(dll_handler);
+				IResult = 1;
+				break;
+			}
+			::FreeLibrary(dll_handler);
 		}
 		else
 		{
-			printf("Getting func ptr is failed \n");
-			::FreeLibrary(dll_handler);
-			return 1;
+			printf("dll isn't loaded \n");
+			IResult = 1;
+			break;
 		}
-		::FreeLibrary(dll_handler);
-	}
-	else
-	{
-		printf("dll isn't loaded \n");
-		return 1;
-	}
-	return 0;
+		return IResult;
+	} 
+	while (true);
 }
 
